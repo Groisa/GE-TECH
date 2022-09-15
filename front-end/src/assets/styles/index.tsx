061c47dda63b961ed5d9a04a12c5dd9880307508
Gabriel Eduardo <gabrieleduardo.s.rosa@gmail.com>
2022-09-15 11:45:26 -0300

    implementa inicio sidemenu

--- front-end/src/assets/styles/index.tsx
+++ front-end/src/assets/styles/index.tsx
@@ -27,22 +27,48 @@ export const DatacontainerHeader = styled.div`
     display: flex;
     justify-content: space-around;
     align-items: center;
-    p{
-        :hover {
-            cursor: pointer;
-            scale: 1.2;
-            text-shadow: 4px 4px 2px rgba(255,255,255, 0.6);
-            transition: 1.5s;
-        }
-    }
-    img {
-        width: 12rem;
-        cursor: pointer;
-        :hover {
-            scale: 1.1;
-            opacity: 90%;
-            transition: 1.5s;
-        }
+    nav {
+        display: flex;
+        width: 100%;
+        justify-content: space-around;
+        align-items: center;
+            div {
+                display: none;
+            }
+            p{
+                :hover {
+                    cursor: pointer;
+                    scale: 1.2;
+                    text-shadow: 4px 4px 2px rgba(255,255,255, 0.6);
+                    transition: 1.5s;
+                }
+            }
+            img {
+                width: 12rem;
+                cursor: pointer;
+                :hover {
+                    scale: 1.1;
+                    opacity: 90%;
+                    transition: 1.5s;
+                }
+            }
+            @media(max-width: 572px){
+                p   {
+                    display: none;
+                }
+                div {
+                    display: block;
+                    font-size: 23px;
+                    padding: 4px;
+                    :active {
+                        background-color: rgba(255,255,255, 0.2) ;
+                        border-radius: 5px;
+                    }
+                }
+                img {
+                    width: 11rem;
+                }
+            }
     }
 `
 export const ImageSource = styled.div`
@@ -53,4 +79,36 @@ export const ImageSource = styled.div`
     background-color: #121283;
     opacity: 30%;
     height: 100vh;
+`
+export const SideMenu = styled.div`
+    height: 100vh;
+    background-color: RGBA(0,0,20,1);
+    width: 100vw;
+    position: absolute;
+    top: 0;
+    nav {
+        div {
+                div {
+                    div {
+                        cursor: pointer;
+                        font-size: 32px;
+                        :active {
+                                background-color: rgba(255,255,255, 0.2) ;
+                                border-radius: 5px;
+                            }  
+                        flex: 0;
+                    }
+                    img {
+                            width: 11rem;
+                            height: 3rem;
+                        }
+                    width: 100%;
+                    align-items: center;
+                    height: 70px;
+                    display: flex;
+                    justify-content: space-evenly;
+                    
+                }
+            }
+    }
 `
--- front-end/src/components/header.tsx
+++ front-end/src/components/header.tsx
@@ -1,23 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { DatacontainerHeader, HeaderContainer, ImageSource } from '../assets/styles'
 
 import Logo from '../assets/imgs/Logobranca.png'
 
+import { FaAlignLeft } from 'react-icons/fa'
+import { SideMenuContainer } from './SideMenuContainer'
+
 export const HeaderComponent: React.FC = () => {
+    const [visible, setVisible] = useState<boolean>(false)
+
+    const handleVisible = () => {
+        setVisible(true)
+    }
     return (
-        <HeaderContainer>
-            <div>
-                <ImageSource />
-            </div>
-            <DatacontainerHeader>
-                <p>Início</p>
-                <p>Sobre</p>
+        <>
+            <HeaderContainer>
+                <div>
+                    <ImageSource />
+                </div>
+                <DatacontainerHeader>
+                    <nav>
+                        <div onClick={handleVisible}>
+                            <FaAlignLeft />
+                        </div>
+                        <p>Início</p>
+                        <p>Sobre</p>
 
-                <img src={Logo} alt='logo da empresa'/>
+                        <img src={Logo} alt='logo da empresa' />
 
-                <p>Serviços</p>
-                <p>Contato</p>
-            </DatacontainerHeader>
-        </HeaderContainer>
+                        <p>Serviços</p>
+                        <p>Contato</p>
+                    </nav>
+                </DatacontainerHeader>
+            </HeaderContainer>
+            <SideMenuContainer visible={visible}/>
+        </>
     )
 }
